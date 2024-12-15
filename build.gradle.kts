c5bb31c251d65a14174e0e352ba87f01258fb434
FaizanBhait <132351196+FaizanBhait@users.noreply.github.com>
2024-12-15 11:04:39 +0500

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.google.gms.google.services) apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
